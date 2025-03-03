28ec4ff50257cfe4a67e0ffbba617e1fc1ce3360
DENIS DE SOUZA <denissouzahehe@gmail.com>
2025-03-02 23:08:02 -0300

    Star dev

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection.Metadata;
+using static System.Console;
+namespace projeto13
+{
+    class Program
+    {
+        static void Main(String[]args)
+        {
+            WriteLine("executando");
+
+            // *
+            // **
+            // ***
+            // ****
+            // *****
+            // ******
+            // *******
+            
+            for(int contadorlinha = 0; contadorlinha < 10; contadorlinha++)
+            
+            {
+
+           
+           for(int contadorcoluna = 0; contadorcoluna < 10; contadorcoluna++)
+           
+            {
+                Write("*");
+              if (contadorcoluna >= contadorlinha)
+              break;
+            }
+                 WriteLine("");
+
+            }
+
+
+            
+
+
+
+
+            for(int contadorlinha = 0; contadorlinha < 10; contadorlinha++)
+            {
+
+            for(int contadorcoluna = 0; contadorcoluna <= contadorlinha; contadorcoluna++)
+            {
+                Write("*");
+               
+            }
+                WriteLine("");
+            }
+            ReadLine();
+
+
+        }
+    }
+
+}
